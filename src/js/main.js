e56ee0c00b02ce526b38451e638628821ece31fa
Anton <aanohin98@gmail.com>
2019-08-16 23:22:53 +0400

    0.6.0
    Added catalog

--- src/js/main.js
+++ src/js/main.js
@@ -102,9 +102,9 @@ function actionPage() {
 
     // фильтр по цене   
     discountCheckbox.addEventListener('click', filter);
-    
-        min.addEventListener('change', filter);
-        max.addEventListener('change', filter);
+
+    min.addEventListener('change', filter);
+    max.addEventListener('change', filter);
 
 
     function filterPrice() {
@@ -139,7 +139,7 @@ function actionPage() {
             }
         });
     }
-    
+
 
     // поиск 
     searchBtn.addEventListener('click', () => {
@@ -180,8 +180,8 @@ function getData() {
             console.warn(err);
             goodsWrapper.innerHTML = '<div style="color: red; font-size: 30px">Упс, что-то пошло не так!</div>';
         });
-        
-  
+
+
 
 }
 
@@ -215,9 +215,39 @@ function renderCards(data) {
 
 function renderCatalog() {
     const cards = document.querySelectorAll('.goods .card');
+    const catalogList = document.querySelector('.catalog-list');
+    const catalogBtn = document.querySelector('.catalog-button');
+    const catalogWrapper = document.querySelector('.catalog');
+    const categories = new Set();
+
     cards.forEach((card) => {
-        console.dir(card.dataset.category);
-    })
+        categories.add(card.dataset.category);
+    });
+
+    categories.forEach((item) => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        catalogList.appendChild(li);
+    });
+
+    catalogBtn.addEventListener('click', (event) => {
+        if (catalogWrapper.style.display) {
+            catalogWrapper.style.display = '';
+        } else {
+            catalogWrapper.style.display = 'block';
+        }
+
+        if (event.target.tagName === 'LI') {
+            cards.forEach((card) => {
+                if (card.dataset.category === event.target.textContent) {
+                    card.parentNode.style.display = '';
+                } else {
+                    card.parentNode.style.display = 'none';
+                }
+            });
+        }
+    });
+    console.log(categories);
 }
 
 
